fe57508c8299b565fbd9534f473867208fd0f5c9
Himanshu <hr1411687@gmail.com>
2025-07-06 18:50:58 +0530

    ok

--- src/app/(workspace)/workspace/layout.tsx
+++ src/app/(workspace)/workspace/layout.tsx
@@ -15,7 +15,7 @@ interface DashboardlayoutProps {
 
 function layout({children}:DashboardlayoutProps) {
   return (
-    <div className='min-h-screen bg-[#121212]'>
+    <div className='min-h-screen bg-white'>
         
         <div className='flex w-full h-full'>
             <div className='fixed left-0 top-0 hidden lg:block lg:w-[264px]  h-full overflow-y-auto'>
@@ -24,7 +24,7 @@ function layout({children}:DashboardlayoutProps) {
             <div className='lg:pl-[264px] w-full'>
 <div className='mx-auto max-w-screen-2xl h-full '>
     <WorkspaceNavbar/>
-    <main className='  h-full bg-[#121212] py-6 px-6 flex flex-col'>
+    <main className='  h-full bg-[#ffff] py-6 px-6 flex flex-col'>
  <NuqsAdapter>
 
 {children}
